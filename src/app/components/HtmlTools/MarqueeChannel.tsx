e4e78ea9d7c34945978c0090b30783784afd5432
youssefiptv936-48 <youssefiptv936@gmail.com>
2026-06-02 18:25:54 +0000

    hhhhh

--- src/app/components/HtmlTools/MarqueeChannel.tsx
+++ src/app/components/HtmlTools/MarqueeChannel.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 // تم إبقاء الـ title فقط وحذف الـ imgSrc تماماً
 interface MarqueeItem {
-  title: string;           
+  title: string;
 }
 
 interface MarqueeProps {
-  channels: MarqueeItem[]; 
+  channels: MarqueeItem[];
 }
 
 export default function MarqueeChannel({ channels }: MarqueeProps) {
@@ -15,20 +15,17 @@ export default function MarqueeChannel({ channels }: MarqueeProps) {
   return (
     /* الإطار الخارجي الثابت للماركي */
     <div className="relative flex w-full overflow-hidden border-b border-t py-6 [direction:ltr]">
-      
+
       {/* المجموعة الأولى - عناوين القنوات الأصلية المفتوحة لعناكب الـ SEO وقوقل */}
       <div className="animate-marquee whitespace-nowrap flex gap-5 ">
         {channels.map((channel, index) => (
           <div key={`origin-${index}`} className="shrink-0">
-            
+
             {/* تصميم الكرت المبني ليعرض العنوان بوضوح في المنتصف */}
-            <div className="relative flex flex-col items-center justify-center     h-24  text-center select-none px-2">
-              
-              {/* العنوان داخل وسم H3 الذي طلبته خصيصاً لتحسين محركات البحث SEO */}
-             <h3 className="text-4xl font-bold text-zinc-200  w-full ">
+            <div className="relative flex items-center justify-center h-24 ">
+              <h3 className="text-3xl font-black uppercase text-white text-center  tracking-tight ">
                 {channel.title}
               </h3>
-
             </div>
 
           </div>
@@ -39,13 +36,11 @@ export default function MarqueeChannel({ channels }: MarqueeProps) {
       <div aria-hidden="true" className="absolute top-6 animate-marquee2 whitespace-nowrap flex gap-5">
         {channels.map((channel, index) => (
           <div key={`clone-${index}`} className="shrink-0">
-            
-            <div className="relative flex flex-col items-center justify-center  h-24  text-center select-none px-2 ">
-              
-              <h3 className="text-4xl font-bold text-zinc-200  w-full  ">
+
+            <div className="relative flex items-center justify-center h-24 ">
+              <h3 className="text-3xl font-black uppercase text-white text-center  tracking-tight ">
                 {channel.title}
               </h3>
-              
             </div>
 
           </div>
